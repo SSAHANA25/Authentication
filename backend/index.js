a071e0def22976a26364328b6d12c7524dc9c2ff
SSAHANA25 <sahanas.23csd@kongu.edu>
2025-06-26 13:53:21 +0530

    updated backend in frontend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://authentication-beryl-six.vercel.app',
   credentials: true
 }));
 app.use(express.json());
@@ -17,7 +17,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false, httpOnly: true }
+  cookie: { secure: true, httpOnly: true }
 }));
 app.use('/api/auth', authRoutes);
 
